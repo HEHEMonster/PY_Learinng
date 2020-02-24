4e8b632825d17bd836f9321c17ab42d9e561b627
apple <apple@appledeMacBook-Pro.local>
2020-02-24 22:32:18 +0800

    first commit

---
+++ first.py
@@ -0,0 +1,15 @@
+# 瞎几把写代码
+name = input('帅哥，你叫什么名字?\n')
+l = len(name)
+print('hi,', name)
+print('你没告诉我你的名字')
+
+# \ 转义
+# r''       原始字符串 
+# """  """  多行
+print(r'////////////Game Over...///////////')
+
+
+# BIF Built-in funcation
+
+# 获取类型的信息  type()   isinstance()
---
+++ loop.py
@@ -0,0 +1,14 @@
+import  re
+# 三元操作符
+#x, y = 1, 3
+#f = x if x > 0 else y
+# print(f)
+
+
+# for 循环
+arr = list(range(1, 10, 2))
+for i in arr:
+    if i == 3:
+        break
+    print(i)
+
