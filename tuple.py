f2dc244f98b089a5964551e99a3308b1f0d9dc19
ken <1030371013@qq.com>
2020-02-25 22:44:48 +0800

    更新学习进度

---
+++ tuple.py
@@ -0,0 +1,21 @@
+# 元祖
+tuple2 = (1, 2, 3, 4)
+
+# 逗号是关键
+tuple3 = 1, 2, 3, 4
+
+tuple4 = (1)
+# type(tuple4) =int
+
+# 创建空元祖
+emptytuple = ()
+
+# 利用切片插入元素
+temp = tuple2[:2]+(2222,) + tuple2[2:0]
+
+
+# 删除元祖
+del tuple2
+
+# 可用逻辑操作符
+# * +  in  not in
